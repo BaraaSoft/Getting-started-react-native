3c47152027470e0f9c0739c487124da3ab7e793e
baraa <baraaabuzeid@hotmail.com>
2019-10-30 08:22:39 +0800

    back card visible

--- Advance/getting-started-rn-adv/src/Collection.js
+++ Advance/getting-started-rn-adv/src/Collection.js
@@ -36,7 +36,7 @@ class Collection extends Component {
     }
 
 
-    componentDidMount() {
+    componentDidUpdate() {
         UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
         LayoutAnimation.spring();
     }
@@ -107,7 +107,7 @@ class Collection extends Component {
                 </Animated.View>
             );
             return (
-                <Animated.View key={item.id} style={[styles.cardStyle, { top: 10 * (index - indx) }]}>
+                <Animated.View key={item.id} style={[styles.cardStyle, { top: 10 * (indx - index) }]}>
                     {renderCard(item)}
                 </Animated.View>
             )
@@ -115,7 +115,7 @@ class Collection extends Component {
     }
     render() {
         return (
-            <View>
+            <View style={{ marginTop: 200 }}>
                 {this.renderCards.call(this)}
             </View>
         );

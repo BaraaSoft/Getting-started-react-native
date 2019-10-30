a82ad2ab70c450164cd7a85b069ab1527b803abc
baraabytes <baraaabuzeid@hotmail.com>
2019-10-30 14:46:49 +0800

    change background

--- Advance/list-slider-rn/App.js
+++ Advance/list-slider-rn/App.js
@@ -15,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#e9e9e9',
+    backgroundColor: '#eeeeff',
     alignItems: 'center',
     justifyContent: 'center',
   },
